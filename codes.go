a1fedc2762924039840d68fe3047265b8bda87b5
Ferry Sutanto <verge@verge-2.local>
2024-04-01 10:42:06 +0700

    fixing error codes

--- codes.go
+++ codes.go
@@ -3,29 +3,25 @@ package errors
 type ErrCode uint
 
 const (
-	ErrorUnknown                      ErrCode = 0
-	ErrNotFound                       ErrCode = 404
-	ErrBadRequest                     ErrCode = 400
-	ErrUnauthorized                   ErrCode = 401
-	ErrForbidden                      ErrCode = 403
-	ErrorConflict                     ErrCode = 409
-	ErrorInternalServer               ErrCode = 500
-	ErrorServiceUnavailable           ErrCode = 503
-	ErrorGatewayTimeout               ErrCode = 504
-	ErrorTooManyRequests              ErrCode = 429
-	ErrorBadRequest                   ErrCode = 400
-	ErrorUnauthorized                 ErrCode = 401
-	ErrorForbidden                    ErrCode = 403
-	ErrorNotFound                     ErrCode = 404
-	ErrorMethodNotAllowed             ErrCode = 405
-	ErrorNotAcceptable                ErrCode = 406
-	ErrorProxyAuthRequired            ErrCode = 407
-	ErrorRequestTimeout               ErrCode = 408
-	ErrorLengthRequired               ErrCode = 411
-	ErrorPreconditionFailed           ErrCode = 412
-	ErrorRequestEntityTooLarge        ErrCode = 413
-	ErrorRequestURITooLong            ErrCode = 414
-	ErrorUnsupportedMediaType         ErrCode = 415
-	ErrorRequestedRangeNotSatisfiable ErrCode = 416
-	ErrUnexpected                     ErrCode = 500
+	ErrUnknown                      ErrCode = 0
+	ErrBadRequest                   ErrCode = 400
+	ErrUnauthorized                 ErrCode = 401
+	ErrForbidden                    ErrCode = 403
+	ErrNotFound                     ErrCode = 404
+	ErrMethodNotAllowed             ErrCode = 405
+	ErrNotAcceptable                ErrCode = 406
+	ErrProxyAuthRequired            ErrCode = 407
+	ErrRequestTimeout               ErrCode = 408
+	ErrConflict                     ErrCode = 409
+	ErrGone                         ErrCode = 410
+	ErrLengthRequired               ErrCode = 411
+	ErrPreconditionFailed           ErrCode = 412
+	ErrRequestEntityTooLarge        ErrCode = 413
+	ErrRequestURITooLong            ErrCode = 414
+	ErrUnsupportedMediaType         ErrCode = 415
+	ErrRequestedRangeNotSatisfiable ErrCode = 416
+	ErrTooManyRequests              ErrCode = 429
+	ErrUnexpected                   ErrCode = 500
+	ErrServiceUnavailable           ErrCode = 503
+	ErrGatewayTimeout               ErrCode = 504
 )
